258df9896cee884cc1575006f93155254833751f
Александр Белькевич <nyaralex@gmail.com>
2018-06-12 19:45:49 +0300

    RECORD

--- src/medcard/sql/shema.sql
+++ src/medcard/sql/shema.sql
@@ -10,3 +10,12 @@ CREATE TABLE CARD
  height INTEGER NOT NULL,
  weight INTEGER NOT NULL
 );
+
+
+CREATE TABLE RECORD
+(id serial PRIMARY KEY,
+ card_id integer REFERENCES CARD (id),
+ create_time timestamp DEFAULT now() NOT NULL,
+ name text NOT NULL,
+ description text NOT NULL
+);
